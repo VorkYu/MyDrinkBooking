f181d1a6ab103e8736f95f4a6482682657e1d37e
vork_yu <yaya79830@gmail.com>
2019-08-26 19:14:32 +0800

    Initial Commit

---
+++ MyDrinkBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDrinkBooking
+//
+//  Created by VorkYu on 2019/8/26.
+//  Copyright © 2019 VorkYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
